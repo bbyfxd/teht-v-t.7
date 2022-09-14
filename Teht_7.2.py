62e37c2072f7dd744b6e33fe444ec61848c1c610
Fadak <thisisfadak@gmail.com>
2022-09-14 13:43:19 +0300

    Commit

--- Teht_7.2.py
+++ Teht_7.2.py
@@ -1,15 +1,13 @@
-# Write a program that asks the user to enter names until he/she enters an empty string.
+nimet = set()
+nimi = input("Anna nimi: ")
+while nimi != "":
+    if nimi in nimet:
+        print("Aiemmin syötetty nimi")
+        nimi = input("Anna uusi nimi: ")
+    else:
+        nimet.add(nimi)
+        print("Uusi nimi.")
+        nimi = input("Anna uusi nimi: ")
 
-keep_going = True
-while keep_going:
-    name = input("Enter names, or enter an empty string to quit: ")
-    if name == "":
-        keep_going = False
-
-# After each name is read the program either prints out New name
-# or Existing name depending on whether the name was entered
-# for the first time.
-
-
-# Finally, the program lists out the input names one by one,
-# one below another in any order. Use the set data structure to store the names.
+for i in nimet:
+    print(i)
